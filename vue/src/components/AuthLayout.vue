28b143b04cb65f71a5b47f4cbe93334a67f78ccf
Sam Samala <satish.samala@ezyvet.com>
2022-12-28 11:14:48 +1300

    partial logout functionality

--- vue/src/components/AuthLayout.vue
+++ vue/src/components/AuthLayout.vue
@@ -1,13 +1,17 @@
 <template>
-
+  <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="w-full max-w-md space-y-8">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
 <script>
+import { LockClosedIcon } from '@heroicons/vue/20/solid'
+
 export default {
-name: "AuthLayout.vue"
+  components: {
+    LockClosedIcon
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
